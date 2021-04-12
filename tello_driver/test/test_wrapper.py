dfa0290bba29389ba90478c92ef65cb1c1e91286
pariaspe <pedroariasperez96@gmail.com>
2021-04-12 12:22:03 +0200

    Added test tello driver from drone wrapper

---
+++ tello_driver/test/test_wrapper.py
@@ -0,0 +1,20 @@
+import time
+from drone_wrapper import DroneWrapper
+
+print("Test started.")
+drone = DroneWrapper()
+drone.takeoff(h=1)
+time.sleep(5)
+
+print("Drone at {0}".format(drone.get_position()[-1]))
+
+drone.set_cmd_pos(z=0.5)
+time.sleep(5)
+print("Drone at {0}".format(drone.get_position()[-1]))
+
+drone.set_cmd_vel(az=0.5)
+time.sleep(5)
+
+drone.land()
+time.sleep(3)
+print("Test finished.")
