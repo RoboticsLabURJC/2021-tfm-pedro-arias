3b5d3359e416838028bea2cf090971deeacf1a6c
pariaspe <pedroariasperez96@gmail.com>
2021-07-05 12:34:37 +0200

    Added mkdir build

--- follow_color/build.sh
+++ follow_color/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd ~/repos/2021-tfm-pedro-arias/follow_color
+mkdir build
 GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$PWD/build
 GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$PWD/models
 cd build
