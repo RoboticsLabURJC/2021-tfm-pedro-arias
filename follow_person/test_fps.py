325fe81e914a0f26bc3389d0ad6e8d415d3c17ce
pariaspe <pedroariasperez96@gmail.com>
2021-10-28 11:25:59 +0200

    Renamed fps test

--- follow_person/test_fps.py
+++ follow_person/test_fps.py
@@ -1,23 +1,33 @@
 import numpy as np
 import cv2
 import yolo_utils
+import time
 
-yolo4_tiny = yolo_utils.YOLOv4Tiny()
-# yolo4 = yolo_utils.YOLOv4()
+#yolo4_tiny = yolo_utils.YOLOv4Tiny()
+yolo4 = yolo_utils.YOLOv4()
 
+n_frames = 0
 cap = cv2.VideoCapture("/dev/video0") # check this
 while(True):
+    if n_frames == 0:
+        t0 = time.time()
+
     # Capture frame-by-frame
     ret, frame = cap.read()
+    n_frames += 1
 
     # Flip verticallly
     frame = cv2.flip(frame, 0)
 
     # Our operations on the frame come here
-    yolo4_tiny.detect_frame(frame)
+    yolo4.detect_frame(frame)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
+
+    # Print FPS
+    print(n_frames/(time.time() - t0))
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
