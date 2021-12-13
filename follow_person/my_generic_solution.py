9a704b02e7227180b9938ee2bba4d7779001a040
Pedro Arias <pedroariasperez96@gmail.com>
2021-12-13 17:08:06 +0100

    Target area made general and added fps info printing

--- follow_person/my_generic_solution.py
+++ follow_person/my_generic_solution.py
@@ -9,7 +9,9 @@ import cv2
 import numpy as np
 import yolo_utils
 import math
+import time
 
+ASPECT_RATIO = 30  # la trigesima parte de la imagen esta ocupada por el area de le persona
 RATE = 50
 
 yolo_pub = rospy.Publisher('/brain/yolo_output/image_raw', Image, queue_size=10)
@@ -114,7 +116,7 @@ def execute(img, label, points):
     height, width, channels = img.shape
     center_image_x = width / 2
     center_image_y = height / 2
-    target_area = 1500
+    target_area = height * width / ASPECT_RATIO
 
     rgb_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
@@ -124,9 +126,10 @@ def execute(img, label, points):
         cx = (abs(points[0]) + abs(points[2]))/2
         cy = (abs(points[1]) + abs(points[3]))/2
 
-        yaw_error = (center_image_x - cx)  # error between the center of the image and the current position of the centroid
-        z_error = (center_image_y - cy)
+        # error between the center of the image and the current position of the centroid
         x_error = (int(area) - target_area)/target_area
+        yaw_error = (center_image_x - cx)
+        z_error = (center_image_y - cy)
 
         cv2.arrowedLine(rgb_img, (int(cx), int(cy)), (int(cx), center_image_y), (255, 0, 0), thickness=3)
         cv2.arrowedLine(rgb_img, (int(cx), int(cy)), (center_image_x, int(cy)), (0, 255, 0), thickness=3)
@@ -140,10 +143,13 @@ def execute(img, label, points):
 
     cmd_pub.publish(bridge.cv2_to_imgmsg(rgb_img, 'bgr8'))
 
+    print("Error", x_error, yaw_error, z_error)
     vx = vx_pid.update(x_error)
     # vy = 0
     yaw_rate = Yr_pid.update(yaw_error)
     vz = vz_pid.update(z_error)
+
+    print("Vels", vx, yaw_rate, vz)
 
     if isclose(vx, 0.0, rel_tol=0.00001):
         vx = 0.0001
@@ -156,13 +162,18 @@ def main():
     drone = DroneWrapper(name=rospy.get_param('/drone/model'))
     yolo4 = yolo_utils.YOLOv4()
 
-    drone.takeoff(h=2, precision=0.2)
+    drone.takeoff(h=1, precision=0.2)
     print("Taken off")
 
     rate = rospy.Rate(RATE)  # 50hz
+
+    # FPS
+    n_frames = 0
+    t0 = time.time()
     while not rospy.is_shutdown():
         try:
             img = drone.get_frontal_image()
+            n_frames += 1
             rgb_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
             yolo4.detect_frame(rgb_img)
             yolo_pub.publish(bridge.cv2_to_imgmsg(rgb_img, 'bgr8'))
@@ -170,6 +181,9 @@ def main():
             label, confidence, points = yolo4.detect_object(img, 'person')
             vx, vy, vz, yaw_rate = execute(img, label, points)
             drone.set_cmd_vel(vx, vy, vz, yaw_rate)
+
+            # Print FPS
+            print("FPS:", n_frames/(time.time() - t0))
         except CvBridgeError:
             print("Error")
             pass
