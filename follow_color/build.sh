dfa4dd8613fbfc25ac3064416d32c8f03822c083
Pedro Arias <pedroariasperez96@gmail.com>
2022-01-31 10:57:35 +0100

    Fixed missing exports

--- follow_color/build.sh
+++ follow_color/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd ~/repos/2021-tfm-pedro-arias/follow_color
 mkdir build
-GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$PWD/build
-GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$PWD/models
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$PWD/build
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$PWD/models
 cd build
 rm -rf *
 cmake ../
